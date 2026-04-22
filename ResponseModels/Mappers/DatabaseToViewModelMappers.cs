bdbc8dcc03fe7d278f7c4b34bafaa4321903afd8
Joanna Wajler <dzo.wajler@gmail.com>
2026-04-22 12:34:32 +0200

    add DatabaseToViewModelMappers class, IDatabaseToViewModel interface with proper implementation and contract

---
+++ ResponseModels/Mappers/DatabaseToViewModelMappers.cs
@@ -0,0 +1,25 @@
+﻿using DbConnection.DbModels;
+using ResponseModels.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponseModels.Mappers
+{
+    public class DatabaseToViewModelMappers: IDatabaseToViewModelMappers
+    {
+        public BookViewModel ToViewModel(Book book)
+        {
+            return new BookViewModel()
+            {
+                Id = book.BookId,
+                Title = book.Title,
+                Author = book.Author,
+                Description = book.Description,
+                Price = book.Price,
+            };
+        }
+    }
+}
---
+++ ResponseModels/Mappers/IDatabaseToViewModelMappers.cs
@@ -0,0 +1,15 @@
+﻿using DbConnection.DbModels;
+using ResponseModels.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponseModels.Mappers
+{
+    public interface IDatabaseToViewModelMappers
+    {
+        BookViewModel ToViewModel(Book book);
+    }
+}
