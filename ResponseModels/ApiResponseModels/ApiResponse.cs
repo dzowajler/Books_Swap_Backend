282325c1b80b5d18e1492026f338e885279e018b
jwajler <dzo.wajler@gmail.com>
2026-06-28 10:35:03 +0200

    add Api Response models for packing response with statusCodes, messages and proper details

---
+++ ResponseModels/ApiResponseModels/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ApiResponseModels
+{
+    public abstract class ApiResponse
+    {
+        public string Code { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ ResponseModels/ApiResponseModels/ProblemDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ApiResponseModels
+{
+    public class ProblemDetails
+    {
+        public string Type { get; set; }
+        public string Title { get; set; }
+        public int Status { get; set; }
+        public string? Detail { get; set; }
+    }
+}
