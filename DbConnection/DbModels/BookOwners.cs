c5c1e75939d04a77e8a57bc0ad9e044e4c2a7e9e
jwajler <dzo.wajler@gmail.com>
2026-07-02 20:42:37 +0200

    Add BookOwners table

---
+++ DbConnection/DbModels/BookOwners.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbConnection.DbModels
+{
+    [Table("BookOwners")]
+    public class BookOwners
+    {
+        [Key]
+        public int UsersBooksId { get; set; }
+
+        public int BookId { get; set; }
+
+        [ForeignKey("BookId")]
+        public Book Book { get; set; }
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User{ get; set; }
+    }
+}
