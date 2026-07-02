11def29cdd932733088076f333c3195ee8a09e49
jwajler <dzo.wajler@gmail.com>
2026-07-02 20:46:50 +0200

    Add Db migrations for creating BookOwners table

---
+++ DbConnection/Migrations/20260628094015_AddUserBooksTable.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbConnection.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserBooksTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UsersBooks",
+                columns: table => new
+                {
+                    UsersBooksId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersBooks", x => x.UsersBooksId);
+                    table.ForeignKey(
+                        name: "FK_UsersBooks_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "BookId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UsersBooks_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersBooks_BookId",
+                table: "UsersBooks",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersBooks_UserId",
+                table: "UsersBooks",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsersBooks");
+        }
+    }
+}
---
+++ DbConnection/Migrations/20260702183714_ChangeUserBooksTableNameToBookOwners.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbConnection.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeUserBooksTableNameToBookOwners : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UsersBooks_Books_BookId",
+                table: "UsersBooks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UsersBooks_Users_UserId",
+                table: "UsersBooks");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UsersBooks",
+                table: "UsersBooks");
+
+            migrationBuilder.RenameTable(
+                name: "UsersBooks",
+                newName: "BookOwners");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UsersBooks_UserId",
+                table: "BookOwners",
+                newName: "IX_BookOwners_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UsersBooks_BookId",
+                table: "BookOwners",
+                newName: "IX_BookOwners_BookId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BookOwners",
+                table: "BookOwners",
+                column: "UsersBooksId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookOwners_Books_BookId",
+                table: "BookOwners",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookOwners_Users_UserId",
+                table: "BookOwners",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookOwners_Books_BookId",
+                table: "BookOwners");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookOwners_Users_UserId",
+                table: "BookOwners");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BookOwners",
+                table: "BookOwners");
+
+            migrationBuilder.RenameTable(
+                name: "BookOwners",
+                newName: "UsersBooks");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BookOwners_UserId",
+                table: "UsersBooks",
+                newName: "IX_UsersBooks_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BookOwners_BookId",
+                table: "UsersBooks",
+                newName: "IX_UsersBooks_BookId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UsersBooks",
+                table: "UsersBooks",
+                column: "UsersBooksId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UsersBooks_Books_BookId",
+                table: "UsersBooks",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UsersBooks_Users_UserId",
+                table: "UsersBooks",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
