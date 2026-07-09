9aafb41ca0c6bf01c74745f7ef702d33ac3c1d47
jwajler <dzo.wajler@gmail.com>
2026-07-09 21:46:27 +0200

    add PriceValidators

---
+++ ValidationService/BooksValidators/PriceValidators.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationService.BooksValidators
+{
+    public static class PriceValidators
+    {
+        public static bool IsPriceGreaterThan0(this decimal price)
+        {
+            return price > 0;
+        }
+
+        public static bool IsPriceLesserThan5000(this decimal price)
+        {
+            return price < 5000;
+        }
+    }
+}
