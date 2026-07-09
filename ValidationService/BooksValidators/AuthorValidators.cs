dcc62a8197ac90f9e1aac87966c42b19a77f51ef
jwajler <dzo.wajler@gmail.com>
2026-07-09 21:46:02 +0200

    add AuthorValidators

---
+++ ValidationService/BooksValidators/AuthorValidators.cs
@@ -0,0 +1,17 @@
+﻿using ResponseModels.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationService.BooksValidators
+{
+    public static class AuthorValidators
+    {
+        public static bool HasAuthorTextContainsAtLeast3Characters(this string author)
+        {
+            return author.Length >= 3;
+        }
+    }
+}
