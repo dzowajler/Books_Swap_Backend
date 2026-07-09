0942c16670117b8598e905d1138360d6d9aa99ff
jwajler <dzo.wajler@gmail.com>
2026-07-09 21:46:39 +0200

    add TitleValidators

---
+++ ValidationService/BooksValidators/TitleValidators.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationService.BooksValidators
+{
+    public static class TitleValidators
+    {
+        public static bool HasTitleContainsAtLeast1Character(this string title)
+        {
+            return title.Length >= 1;
+        } 
+    }
+}
