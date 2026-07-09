c92c154ee2b3f98a2476eef415440835d2ef3962
jwajler <dzo.wajler@gmail.com>
2026-07-09 21:49:49 +0200

    add BookValidatorService, IBookValidatorService with it's implementations

---
+++ ValidatorService/BookValidatorService.cs
@@ -0,0 +1,55 @@
+﻿using ResponseModels.ViewModels;
+using System.Runtime.CompilerServices;
+using ValidationService.BooksValidators;
+
+namespace ValidatorService
+{
+    public class BookValidatorService : IBookValidatorService
+    {
+        private Dictionary<string, Func<bool>> _bookValidationErrorDictionary;
+
+        public BookValidatorService()
+        {
+
+        }
+
+        public IEnumerable<string> ValidateBook(BookViewModel bookViewModel)
+        {
+            SetValidationErrorDictionary(bookViewModel);
+
+            LeaveDictRecordsWhichDontPassValidation();
+
+            if (_bookValidationErrorDictionary.Count != 0)
+                return ExtractValidationErrors();
+
+            return Enumerable.Empty<string>();
+        }
+
+        private IEnumerable<string> ExtractValidationErrors()
+        {
+            return _bookValidationErrorDictionary.Select(item => item.Key).ToList();
+        }
+
+        private void LeaveDictRecordsWhichDontPassValidation()
+        {
+            foreach (var item in _bookValidationErrorDictionary)
+            {
+                if (item.Value.Invoke() == true)
+                    _bookValidationErrorDictionary.Remove(item.Key);
+            }
+        }
+
+        private void SetValidationErrorDictionary(BookViewModel bookViewModel)
+        {
+            _bookValidationErrorDictionary = new Dictionary<string, Func<bool>>()
+            {
+                { ValidationErrorMessages.TooShortAuthorName,
+                    bookViewModel.Author.HasAuthorTextContainsAtLeast3Characters },
+                { ValidationErrorMessages.TooShortDescription,
+                    bookViewModel.Description.HasDescriptionFieldContainAtLeast20Characters },
+                { ValidationErrorMessages.TooShortTitle,
+                   bookViewModel.Title.HasTitleContainsAtLeast1Character },
+            };
+        }
+    }
+}
---
+++ ValidatorService/IBookValidatorService.cs
@@ -0,0 +1,14 @@
+﻿using ResponseModels.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatorService
+{
+    public interface IBookValidatorService
+    {
+        IEnumerable<string> ValidateBook(BookViewModel bookViewModel);
+    }
+}
