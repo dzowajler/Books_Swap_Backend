c750de77457015840361401cf81d63f30262b883
jwajler <dzo.wajler@gmail.com>
2026-07-09 21:52:02 +0200

    add ValidationErrorMessages static class with static properties about description errors

---
+++ ValidatorService/ValidationErrorMessages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatorService
+{
+    public static class ValidationErrorMessages
+    {
+        public static string TooShortAuthorName
+            = "Author field is too short. It should contain at least 3 characters";
+
+        public static string TooShortTitle
+            = "Title is too short. It should contain at least one character";
+
+        public static string TooShortDescription
+            = "Description is too short. It should contain at least 20 characters";
+
+        public static string PriceIsOutOfLowerRange
+            = "Price is out of range. It should be greater than 0.00";
+
+        public static string PriceIsOutOfUpperRange
+            = "Price is out of range. It should be lower than 5000.00";
+    }
+}
