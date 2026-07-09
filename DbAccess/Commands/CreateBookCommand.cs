9b1982f667edde0637e13f7e8a5f832012687079
jwajler <dzo.wajler@gmail.com>
2026-07-09 21:42:25 +0200

    add CreateBookCommandClass

---
+++ DbAccess/Commands/CreateBookCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAccess.Commands
+{
+    public class CreateBookCommand
+    {
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+    }
+}
