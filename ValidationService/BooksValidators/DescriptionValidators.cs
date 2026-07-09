c6e80e09201d8aca06563017293e4fe85f600359
jwajler <dzo.wajler@gmail.com>
2026-07-09 21:46:15 +0200

    add DescriptionValidators

---
+++ ValidationService/BooksValidators/DescriptionValidators.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationService.BooksValidators
+{
+    public static class DescriptionValidators
+    {
+        public static bool HasDescriptionFieldContainAtLeast20Characters(this string description)
+        {
+            return description.Length >= 20;
+        }
+    }
+}
