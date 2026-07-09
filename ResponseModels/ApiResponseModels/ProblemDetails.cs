00bc4cbc5a32c6b8f9d87c11754c385ad77f9c73
jwajler <dzo.wajler@gmail.com>
2026-07-09 21:44:31 +0200

    correct Details property to IEnumerable<string>

--- ResponseModels/ApiResponseModels/ProblemDetails.cs
+++ ResponseModels/ApiResponseModels/ProblemDetails.cs
@@ -11,6 +11,6 @@ namespace Models.ApiResponseModels
         public string Type { get; set; }
         public string Title { get; set; }
         public int Status { get; set; }
-        public string? Detail { get; set; }
+        public IEnumerable<string> Details { get; set; }
     }
 }
