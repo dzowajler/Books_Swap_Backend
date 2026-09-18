b4f0c7b9d954c2ac91373845c53f51edf171b875
jwajler <dzo.wajler@gmail.com>
2026-09-18 13:16:55 +0200

    add PhotoPath and Price field

--- ResponseModels/ViewModels/BookViewModel.cs
+++ ResponseModels/ViewModels/BookViewModel.cs
@@ -10,9 +10,10 @@ namespace ResponseModels.ViewModels
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
         public string Author { get; set; }
-        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
         public string Genre { get; set; }
+        public string PhotoPath { get; set; }
     }
 }
